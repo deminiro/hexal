39e7ad5062a4d334fcfa324e4705dc5ab07385fd
deminiro <43145879+deminiro@users.noreply.github.com>
2019-05-23 11:38:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-(function animation() {
 	const elem = document.getElementById("canvas");
 	const ctx = elem.getContext("2d");
 	const div = document.getElementById('precentage');
  
-	ctx.beginPath();
-	ctx.moveTo(10,0);
-	ctx.lineTo(10,200);
-	ctx.stroke();
-
 	function drawCircle(x){
   		ctx.beginPath();
   		ctx.arc(x,10,10,0,2*Math.PI);
   		ctx.fillStyle="#35BBE0";
   		ctx.fill();
 	}
-
-	let num = 0;
 	let x = 0;
-
 	setInterval(function(){ 
-  		if(num < 100) {
-  		ctx.clearRect(0,0,200,200);
+  		if(x < 200) {
+  		ctx.clearRect(0,0,200,20);
   		drawCircle(x%200);
   		x++;
-  		num+=0.5;
-  		div.innerText = `${Math.floor(num)}%`;
+  		div.innerText = `${Math.floor(x/2)}%`;
   		}
-  		if(num === 100) {
+  		if(x === 200) {
     	elem.style.display = 'none';
 		div.style.display = 'none';
-		document.getElementById('header').style.display = '';
-		document.getElementById('main').style.display = '';
-		document.getElementById('footer').style.display = ''; 
+		document.getElementById('header').style.opacity = '1';
+		document.getElementById('main').style.opacity = '1';
+		document.getElementById('footer').style.opacity = '1'; 
 		clearInterval();
   		};
-	}, 20);
-}());
+	}, 10);
